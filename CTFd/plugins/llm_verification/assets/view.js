44bd20a6d4ced125c3601e54b4e0b5268b97e329
Brooke <12855764+goingforbrooke@users.noreply.github.com>
2023-07-06 21:25:26 -0700

    Fix: Show previous submissions
    This fix rewrites the custom-defined `Moment` function so it
    behaves as expected.
    
    The issue was caused by an issue in the custom-defined `Moment`
    function in `view.js`. When the JS code wanted to create a
    "card" for a challenge submission (under the "Awarded" category,
    for example), calling this function would fail, the card wouldn't
    be added, and no further categories (such as "Pending" and
    "Incorrect" would be added.

--- CTFd/plugins/llm_verification/assets/view.js
+++ CTFd/plugins/llm_verification/assets/view.js
@@ -8,39 +8,38 @@ if (CTFd.lib.$) {
   $ = CTFd.lib.$;
 }
 
-if (!window.Moment) {
-  Date.prototype.fromNow = function() {
-    var date = this;
-    var delta = Math.round((+new Date() - date) / 1000);
+function Moment(d) {
+  var date = new Date(d);
+  date.fromNow = function() {
+      var delta = Math.round((+new Date() - this) / 1000);
 
-    var minute = 60,
+      var minute = 60,
       hour = minute * 60,
       day = hour * 24,
       week = day * 7;
 
-    var fuzzy;
+      var fuzzy;
 
-    if (delta < 30) {
-      fuzzy = "just now";
-    } else if (delta < minute) {
-      fuzzy = delta + " seconds ago.";
-    } else if (delta < 2 * minute) {
-      fuzzy = "a minute ago";
-    } else if (delta < hour) {
-      fuzzy = Math.floor(delta / minute) + " minutes ago";
-    } else if (Math.floor(delta / hour) == 1) {
-      fuzzy = "1 hour ago";
-    } else if (delta < day) {
-      fuzzy = Math.floor(delta / hour) + " hours ago";
-    } else if (delta < day * 2) {
-      fuzzy = "yesterday";
-    }
-    return fuzzy;
+      if (delta < 30) {
+          fuzzy = "just now";
+      } else if (delta < minute) {
+          fuzzy = delta + " seconds ago.";
+      } else if (delta < 2 * minute) {
+          fuzzy = "a minute ago";
+      } else if (delta < hour) {
+          fuzzy = Math.floor(delta / minute) + " minutes ago";
+      } else if (Math.floor(delta / hour) == 1) {
+          fuzzy = "1 hour ago";
+      } else if (delta < day) {
+          fuzzy = Math.floor(delta / hour) + " hours ago";
+      } else if (delta < day * 2) {
+          fuzzy = "yesterday";
+      }
+      return fuzzy;
   };
-  function Moment(d) {
-    return new Date(d);
-  }
+  return date;
 }
+
 
 async function generate_text(challenge_id, prompt) {
   var domain = CTFd.config.urlRoot;
