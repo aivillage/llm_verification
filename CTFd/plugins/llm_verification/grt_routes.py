5e2eaf6c1497c84477da7b27cd5c2bd005e876a7
Brooke <12855764+goingforbrooke@users.noreply.github.com>
2023-07-08 16:21:04 -0700

    Move user/team-specific query setup into `utils.py`

--- CTFd/plugins/llm_verification/grt_routes.py
+++ CTFd/plugins/llm_verification/grt_routes.py
@@ -8,15 +8,15 @@ from requests.exceptions import HTTPError
 # CTFd imports.
 from CTFd.models import Awards, Challenges, Fails, Solves, Submissions, db
 from CTFd.plugins import bypass_csrf_protection
-from CTFd.utils import get_config
 from CTFd.utils.dates import isoformat
 from CTFd.utils.decorators import admins_only, authed_only
-from CTFd.utils.modes import USERS_MODE, TEAMS_MODE, get_model
+from CTFd.utils.modes import get_model
 from CTFd.utils.user import get_current_user
 
 # LLM Verification Plugin module imports.
 from .grt_models import GRTSubmission, GRTSolves, LlmChallenge, Pending, Awarded
 from .remote_llm import generate_text
+from .utils import get_filter_by_mode
 
 
 log = getLogger(__name__)
@@ -62,22 +62,8 @@ def add_routes() -> Blueprint:
                                                                         'extracted_results': None}}
         # For each answer submission of a given type (e.g. "Pending", "Solves", "Awarded", "Fails")...
         for ctfd_model in answer_submissions:
-            # If CTFd's configured for "users..."
-            if get_config('user_mode') == USERS_MODE:
-                # ... then define a query filter for the "user" `USER_MODE`.
-                mode_uid = ctfd_model.user_id
-                current_uid = get_current_user().id
-            # Otherwise, if CTFd's configured for "teams..."
-            elif get_config('user_mode') == TEAMS_MODE:
-                # ... then define a query filter for the "team" `USER_MODE`.
-                mode_uid = ctfd_model.team_id
-                current_uid = get_current_user().team_id
-            # Otherwise, if CTFd's configured for neither "users" nor "teams"...
-            else:
-                # ... then raise an error.
-                raise ValueError(f'Invalid user mode: "{get_config("user_mode")}" '
-                                 f'is not "{USERS_MODE}" '
-                                 f'or "{TEAMS_MODE}"')
+            # Define query filters for the current user/team and the current user/team's ID.
+            mode_uid, current_uid = get_filter_by_mode(ctfd_model)
             # Query the database for the user's answer submissions for this challenge.
             answer_submissions[ctfd_model]['query_results'] = ctfd_model.query.filter(mode_uid == current_uid,
                                                                                       ctfd_model.challenge_id == challenge_id).all()
---
+++ CTFd/plugins/llm_verification/utils.py
@@ -0,0 +1,34 @@
+"""Utilities for the LLM Verification plugin."""
+# CTFd imports.
+from CTFd.utils import get_config
+from CTFd.utils.modes import USERS_MODE, TEAMS_MODE
+from CTFd.utils.user import get_current_user
+
+
+def get_filter_by_mode(ctfd_model):
+    """Get a query filter for the current user/team.
+
+    Arguments:
+        ctfd_model: The CTFd model to use for the query filter.
+
+    Returns:
+        tuple (mode_uid, current_uid): A query filter for the current user/team and the current 
+            user/team's ID.
+    """
+    # If CTFd's configured for "users..."
+    if get_config('user_mode') == USERS_MODE:
+        # ... then define a query filter for the "user" `USER_MODE`.
+        mode_uid = ctfd_model.user_id
+        current_uid = get_current_user().id
+    # Otherwise, if CTFd's configured for "teams..."
+    elif get_config('user_mode') == TEAMS_MODE:
+        # ... then define a query filter for the "team" `USER_MODE`.
+        mode_uid = ctfd_model.team_id
+        current_uid = get_current_user().team_id
+    # Otherwise, if CTFd's configured for neither "users" nor "teams"...
+    else:
+        # ... then raise an error.
+        raise ValueError(f'Invalid user mode: "{get_config("user_mode")}" '
+                            f'is not "{USERS_MODE}" '
+                            f'or "{TEAMS_MODE}"')
+    return mode_uid, current_uid
