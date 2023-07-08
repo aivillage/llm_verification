967d52407b896f12fa0f6b668cff0cba78d27f6f
Brooke <12855764+goingforbrooke@users.noreply.github.com>
2023-07-07 21:15:58 -0700

    Refactor: Dynamically configure team/user filters

--- CTFd/plugins/llm_verification/grt_routes.py
+++ CTFd/plugins/llm_verification/grt_routes.py
@@ -11,7 +11,7 @@ from CTFd.plugins import bypass_csrf_protection
 from CTFd.utils import get_config
 from CTFd.utils.dates import isoformat
 from CTFd.utils.decorators import admins_only, authed_only
-from CTFd.utils.modes import USERS_MODE, get_model
+from CTFd.utils.modes import USERS_MODE, TEAMS_MODE, get_model
 from CTFd.utils.user import get_current_user
 
 # LLM Verification Plugin module imports.
@@ -53,16 +53,32 @@ def add_routes() -> Blueprint:
         log.debug(f'User "{current_user.name}" '
                   f'requested their answer submissions for challenge "{challenge_id}"')
         log.debug(f'Configured user mode: "{get_config("user_mode")}"')
-        log.debug(f'Current user mode: "{USERS_MODE}"')
+        log.debug(f'Current user mode (can be "team" or "user"): "{USERS_MODE}"')
         submission_mappings = {Pending: None, Solves: None, Awarded: None, Fails: None}
         for ctfd_model in submission_mappings:
             # If CTFd's configured for "users..."
             if get_config('user_mode') == USERS_MODE:
-                submission_mappings[ctfd_model] = ctfd_model.query.filter(ctfd_model.user_id == current_user.id,
+                # ... then define a query filter for the "user" `USER_MODE`.
+                model_filter = ctfd_model.user_id
+                user_filter = current_user.id
+            # Otherwise, if CTFd's configured for "teams..."
+            elif get_config('user_mode') == TEAMS_MODE:
+                # ... then define a query filter for the "team" `USER_MODE`.
+                model_filter = ctfd_model.team_id
+                user_filter = current_user.team_id
+            # Otherwise, if CTFd's configured for neither "users" nor "teams"...
+            else:
+                # ... then raise an error.
+                raise ValueError(f'Invalid user mode: "{get_config("user_mode")}" '
+                                    f'is not "{USERS_MODE}" '
+                                    f'or "{TEAMS_MODE}"')
+            # If CTFd's configured for "users..."
+            if get_config('user_mode') == USERS_MODE:
+                submission_mappings[ctfd_model] = ctfd_model.query.filter(model_filter == user_filter,
                                                                           ctfd_model.challenge_id == challenge_id).all()
             # Otherwise, assuming that CTFd's configured for "teams..."
             else:
-                submission_mappings[ctfd_model] = ctfd_model.query.filter(ctfd_model.team_id == current_user.team_id,
+                submission_mappings[ctfd_model] = ctfd_model.query.filter(model_filter == user_filter,
                                                                           ctfd_model.challenge_id == challenge_id).all()
         query_results = {Pending: None, Solves: None, Awarded: None, Fails: None}
         for ctfd_model in submission_mappings:
